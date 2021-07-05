6e231d61751626438af7298b38f89cea959dee5d
John <1019178@student.sae.edu.au>
2021-07-05 15:51:12 +1000

    Class Updates

--- Assets/Team Members/NidgyWidgy/Scripts/NetworkManagerHnS.cs
+++ Assets/Team Members/NidgyWidgy/Scripts/NetworkManagerHnS.cs
@@ -3,9 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Mirror;
-using Mono.CecilX;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 namespace Nidgy
