9527901870f13292b8ba545c6fee9f4ee51f62cf
John <1019178@student.sae.edu.au>
2021-07-08 00:20:39 +1000

    GameManager Updated

--- Assets/Team Members/NidgyWidgy/Scripts/GameManager.cs
+++ Assets/Team Members/NidgyWidgy/Scripts/GameManager.cs
@@ -8,10 +8,18 @@ public class GameManager : MonoBehaviour
  private bool RandomRole;
 
  public event Action StartGame;
+ public event Action EndGame;
 
  public void StartRound()
  {
    StartGame?.Invoke(); 
+   Debug.Log("The round has started");
+ }
+
+ public void EndRound()
+ {
+     EndGame?.Invoke();
+     Debug.Log("The round is over");
  }
  
 }
