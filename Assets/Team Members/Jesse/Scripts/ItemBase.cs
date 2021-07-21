5d0f3ceca0d8e9a05311924887bcf975fe404c7d
Jesse <jesseanderton@hotmail.com>
2021-07-21 20:26:39 +1000

    Starting On The Item Pickup

---
+++ Assets/Team Members/Jesse/Scripts/ItemBase.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    [Tooltip("The speed of the cube rotation.")]
+    public float spinSpeed;
+
+    [Tooltip("The height of the bounce (Wave Amplitude.)")]
+    public float bounceHeight = 0.5f;
+
+    [Tooltip("The amount of bounces (Wave Frequency.)")]
+    public float bounceSpeed = 1f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(spinSpeed, spinSpeed, spinSpeed);
+
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * bounceSpeed) * bounceHeight;
+                
+        transform.position = tempPos;
+    }
+}
