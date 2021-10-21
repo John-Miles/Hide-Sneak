42ac7bd07be1eecc7e237f895d514dbf0b18017f
John <1019178@student.sae.edu.au>
2021-10-21 18:31:36 +1100

    Compile Work

--- Assets/Team Members/John/Scripts/EscapePoint.cs
+++ Assets/Team Members/John/Scripts/EscapePoint.cs
@@ -7,20 +7,6 @@ public class EscapePoint : MonoBehaviour
 {
     public GameManager gameManager;
 
-    public void OnEnable()
-    {
-        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Thief"))
-        {
-            gameManager.ThiefEscaped(other.gameObject);
-        }
-    }
-
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
