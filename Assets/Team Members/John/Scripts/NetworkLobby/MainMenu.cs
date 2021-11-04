fd6f22b327488710f7e9b4cec77a7f5d260fb020
Nidge <1013734@student.sae.edu.au>
2021-11-04 13:16:52 +1100

    settings progress

--- Assets/Team Members/John/Scripts/NetworkLobby/MainMenu.cs
+++ Assets/Team Members/John/Scripts/NetworkLobby/MainMenu.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror.Examples.Chat;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace John
 {
@@ -15,6 +18,15 @@ namespace John
         [Header("Settings")]
         public AudioMixer audioMixer;
 
+        public TMP_Text masterValueText;
+        public TMP_Text musicValueText;
+        public TMP_Text effectsValueText;
+
+        public Slider mouseSensX;
+        public TMP_Text xValue;
+        public Slider mouseSensY;
+        public TMP_Text yValue;
+
         public void HostLobby()
         {
             networkManager.StartHost();
@@ -22,10 +34,46 @@ namespace John
             landingPagePanel.SetActive(false);
         
         }
-       public void SetVolume(float volume)
+
+        #region AUDIO
+        
+       public void SetMasterVolume(float volume)
        {
            audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+
+          // masterValueText.text = audioMixer.GetFloat("MasterVolume");
        }
+
+       public void SetMusicVolume(float volume)
+       {
+           audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+       }
+
+       public void SetEffectsVolume(float volume)
+       {
+           audioMixer.SetFloat("EffectsVolume", Mathf.Log10(volume) * 20);
+       }
+       
+       #endregion
+
+       #region Controls
+
+       public void SetMouseSensitivityX()
+       {
+           PlayerPrefs.SetFloat("MouseSensX", mouseSensX.value);
+           
+           xValue.text = mouseSensX.value.ToString("F");
+       }
+
+       public void SetMouseSensitivityY()
+       {
+           PlayerPrefs.SetFloat("MouseSensY", mouseSensY.value);
+           
+           yValue.text = mouseSensY.value.ToString("F");
+       }
+       
+
+       #endregion
         public void QuitGame()
         {
             Application.Quit();
