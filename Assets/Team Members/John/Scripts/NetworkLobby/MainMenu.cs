77409eb548a43cfc6925d264f95bb8836cc21cc8
Nidge <1013734@student.sae.edu.au>
2021-11-09 11:35:19 +1100

    resolution dropdown fix

--- Assets/Team Members/John/Scripts/NetworkLobby/MainMenu.cs
+++ Assets/Team Members/John/Scripts/NetworkLobby/MainMenu.cs
@@ -41,7 +41,7 @@ namespace John
             int currentResolutionIndex = 0;
             for (int i = 0; i < resolutions.Length; i++)
             {
-                string option = resolutions[i].width + "x" + resolutions[i].height;
+                string option = resolutions[i].width + "x" + resolutions[i].height + " " + resolutions[i].refreshRate + "Hz";
                 options.Add(option);
 
                 if (resolutions[i].width == Screen.width &&
