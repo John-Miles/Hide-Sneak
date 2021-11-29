c47f1cc0108625982299f31fb42088b5d9c19557
Nidge <1013734@student.sae.edu.au>
2021-11-29 12:56:53 +1100

    thief animation work, moved camera to match model head location

---
+++ Assets/Animations/animStateController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class animStateController : MonoBehaviour
+{
+    private Animator animator;
+
+    private float velocityZ = 0.0f;
+
+    private float velocityX = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool backwardPressed = Input.GetKey(KeyCode.S);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+
+        if (forwardPressed)
+        {
+            velocityZ = 1;
+        }
+        if (backwardPressed)
+        {
+            velocityZ = -1;
+        }
+        if (leftPressed)
+        {
+            velocityX = -1;
+        }
+        if (rightPressed)
+        {
+            velocityX = 1;
+        }
+
+        else
+        {
+            velocityX = 0;
+            velocityZ = 0;
+        }
+        animator.SetFloat("VelZ", velocityZ);
+        animator.SetFloat("VelX", velocityX);
+    }
+}
